150d88c84091468f5b503a2576142e7e8a079374
Andrew Tsao <andrew.tsao8@gmail.com>
2016-09-15 11:53:13 -0700

    (init) Updates app.js to be non-connected component

--- client/components/app.js
+++ client/components/app.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className='wrapper'>
@@ -10,5 +9,3 @@ class App extends Component {
     );
   }
 }
-
-export default connect(null, null)(App);
